91fe80e0d7d1081a8e9a26ec3450397ab703f484
Ryan Thames <ryanthames@gmail.com>
2017-01-29 08:33:59 -0600

    Finish initial pass at encoding

--- elixir/run-length-encoding/rle.exs
+++ elixir/run-length-encoding/rle.exs
@@ -1,15 +1,24 @@
 defmodule RunLengthEncoder do
   def encode(string) do
-    string
-    |> String.graphemes
-    |> _encode("", 0, "")
-    |> List.to_string
+    cond do
+      String.length(string) == 0 ->
+        ""
+      true ->
+        [first | rest] = string |> String.graphemes
+        rest |> _encode(first, 1, "")
+    end
   end
 
   def decode(string) do
   end
 
   defp _encode([current|[]], last, current_count, encoded_string) do
+    cond do
+      current == last ->
+        encoded_string <> "#{current_count+1}#{last}"
+      true ->
+        encoded_string <> "#{current_count}#{last}1#{current}"
+    end
   end
 
   defp _encode([current|rest], last, current_count, encoded_string) do
@@ -17,7 +26,7 @@ defmodule RunLengthEncoder do
       current == last -> 
         _encode(rest, current, current_count + 1, encoded_string)
       true ->
-        _encode(rest, current, 0, encoded_string <> "#{current_count}#{last}")
+        _encode(rest, current, 1, encoded_string <> "#{current_count}#{last}")
     end
   end
 end

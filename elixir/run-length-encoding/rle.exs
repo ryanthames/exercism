ec7738d9daba47892d8b75012fbfafb22e532158
Ryan Thames <ryan.thames@apexcapitalcorp.com>
2017-01-30 08:49:08 -0600

    Improve encode function

--- elixir/run-length-encoding/rle.exs
+++ elixir/run-length-encoding/rle.exs
@@ -1,39 +1,23 @@
 defmodule RunLengthEncoder do
   def encode(string) do
-    cond do
-      String.length(string) == 0 ->
-        ""
-      true ->
-        [first | rest] = string |> String.graphemes
-        rest |> _encode(first, 1, "")
-    end
-  end
-
-  defp _encode([current|[]], last, current_count, encoded_string) do
-    cond do
-      current == last ->
-        encoded_string <> "#{current_count+1}#{last}"
-      true ->
-        encoded_string <> "#{current_count}#{last}1#{current}"
-    end
-  end
-
-  defp _encode([current|rest], last, current_count, encoded_string) do
-    cond do
-      current == last -> 
-        _encode(rest, current, current_count + 1, encoded_string)
-      true ->
-        _encode(rest, current, 1, encoded_string <> "#{current_count}#{last}")
-    end
+    string
+    |> String.graphemes
+    |> Stream.chunk_by(fn x -> x end)
+    |> Stream.map(&collapse(&1))
+    |> Enum.join
   end
 
   def decode(string) do
     Regex.scan(~r/([0-9]+[A-Z])/, string)
-    |> Enum.map(&flatten(&1))
+    |> Enum.map(&expand(&1))
     |> List.to_string
   end
 
-  defp flatten([e|_]) do
+  defp collapse(s) do
+    "#{Enum.count(s)}#{Enum.at(s, 0)}"
+  end
+
+  defp expand([e|_]) do
     [count] = Regex.run(~r/[0-9]+/, e)
     [letter] = Regex.run(~r/[A-Z]/, e)
 

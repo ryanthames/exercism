99434f90f621b03a31c85ca693cca451971dadf3
Ryan Thames <ryan.thames@apexcapitalcorp.com>
2017-01-31 14:50:10 -0600

    Finish scrabble

--- elixir/scrabble-score/scrabble.exs
+++ elixir/scrabble-score/scrabble.exs
@@ -4,6 +4,22 @@ defmodule Scrabble do
   """
   @spec score(String.t) :: non_neg_integer
   def score(word) do
+    word
+    |> String.upcase
+    |> String.graphemes
+    |> Enum.reduce(0, fn(s, acc) -> acc + calculate_letter_score(s) end)
+  end
 
+  defp calculate_letter_score(s) do
+    cond do
+      String.contains?("AEIOULNRST", s) -> 1
+      String.contains?("DG", s) -> 2
+      String.contains?("BCMP", s) -> 3
+      String.contains?("FHVWY", s) -> 4
+      "K" === s -> 5
+      String.contains?("JX", s) -> 8
+      String.contains?("QZ", s) -> 10
+      true -> 0
+    end
   end
 end

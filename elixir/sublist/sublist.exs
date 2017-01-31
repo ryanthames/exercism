825714061f7dc4df977d770a398b261d46a71c8d
Ryan Thames <ryan.thames@apexcapitalcorp.com>
2017-01-31 07:17:53 -0600

    Sublist WIP

---
+++ elixir/sublist/sublist.exs
@@ -0,0 +1,24 @@
+defmodule Sublist do
+  @doc """
+  Returns whether the first list is a sublist or a superlist of the second list
+  and if not whether it is equal or unequal to the second list.
+  """
+  def compare(a, b) do
+    cond do
+      a == b -> :equal
+      is_sublist(a, b) -> :sublist
+      is_sublist(b, a) -> :superlist
+      true -> :unequal
+    end
+  end
+
+  defp is_sublist(_a, []), do: false
+
+  defp is_sublist(a, b) do
+    a_size = Enum.count(a)
+    cond do
+      a == Enum.take(b, a_size) -> true
+      true -> is_sublist(a, Enum.drop(b, a_size))
+    end
+  end
+end

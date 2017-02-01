52f09598b7805466521a72d8af6e1b42d179694b
Ryan Thames <ryan.thames@apexcapitalcorp.com>
2017-02-01 07:08:31 -0600

    Iteration 1 of sum of multiples

--- elixir/sum-of-multiples/sum_of_multiples.exs
+++ elixir/sum-of-multiples/sum_of_multiples.exs
@@ -4,6 +4,21 @@ defmodule SumOfMultiples do
   """
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
   def to(limit, factors) do
+    1..limit-1
+    |> Enum.reduce(0, fn(x, acc) -> 
+      cond do
+        multiple?(x, factors) -> acc + x
+        true -> acc
+      end
+    end)
+  end
 
+  defp multiple?(x, []), do: false
+
+  defp multiple?(x, [factor | rest]) do
+    cond do
+      rem(x, factor) === 0 -> true
+      true -> multiple?(x, rest)
+    end
   end
 end

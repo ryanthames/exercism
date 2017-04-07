261b78e56d9a0cfb12f8b082f7c1b841195e20c2
Ryan Thames <ryanthames@gmail.com>
2017-04-07 18:12:45 -0500

    Finish protein translation problem

--- elixir/protein-translation/protein_translation.exs
+++ elixir/protein-translation/protein_translation.exs
@@ -1,9 +1,38 @@
 defmodule ProteinTranslation do
+  @protein_mapping %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
+
   @doc """
   Given an RNA string, return a list of proteins specified by codons, in order.
   """
   @spec of_rna(String.t()) :: { atom,  list(String.t()) }
   def of_rna(rna) do
+    result = rna
+    |> String.graphemes
+    |> Enum.chunk(3)
+    |> Enum.map(&List.to_string(&1))
+    |> Enum.map(&convert_codon(&1))
+    |> Enum.chunk_by(&(&1 == "STOP"))
+    |> List.first
+
+    {:ok, result}
   end
 
   @doc """
@@ -29,24 +58,10 @@ defmodule ProteinTranslation do
   """
   @spec of_codon(String.t()) :: { atom, String.t() }
   def of_codon(codon) do
-    codon
-    |> String.graphemes
-    |> Enum.chunk(3)
-    |> Enum.map(&List.to_string(&1))
-    |> process_codon_list
-  end
-
-  defp process_codon_list(codons) when length(codons) == 1 do
-  {:ok, convert_codon(List.first(codons))}
-  end
-
-  defp process_codon_list(codons) do
-    result = codons |> Enum.map(&convert_codon(&1))
-    {:ok, result}
+    {:ok, convert_codon(codon)}
   end
 
   defp convert_codon(codon) do
-    "Methionine"
+    Map.get(@protein_mapping, codon)
   end
 end
-

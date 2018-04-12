21cc0f2ce5e888c2e778191f26f3b254941c5d5a
Ryan Thames <ryanthames@gmail.com>
2018-04-12 18:29:30 -0500

    Clojure - Armstrong numbers

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,3 +1,9 @@
 (ns armstrong-numbers)
 
-(defn armstrong? [n])
+(defn explode-to-digits [n]
+  (map #(Character/digit % 10) (str n)))
+
+(defn armstrong? [n]
+  (let [digits (explode-to-digits n)
+        digit-count (count digits)]
+    (= n (reduce #(+ %1 (reduce * (repeat digit-count %2))) 0 digits))))

e8b5d951155c68381effa8085226dbc4414fa054
Ryan Thames <ryanthames@gmail.com>
2018-12-26 18:34:20 -0600

    Isogram exercise - first pass

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -1,7 +1,22 @@
-class IsogramChecker {
+import java.util.ArrayList;
+import java.util.List;
 
-    boolean isIsogram(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+class IsogramChecker {
+  boolean isIsogram(String phrase) {
+    List<Character> charList = new ArrayList<>();
+
+    for (int i = 0; i < phrase.length(); i++) {
+      char c = phrase.toUpperCase().charAt(i);
+
+      if (' ' != c && '-' != c) {
+        if (charList.contains(c)) {
+          return false;
+        } else {
+          charList.add(c);
+        }
+      }
     }
 
+    return true;
+  }
 }
